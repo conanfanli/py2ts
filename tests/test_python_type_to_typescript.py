ac2802ce7a745ab6ab33677494a09b4e3c02c803
Conan Li <conanlics@gmail.com>
2020-04-11 10:59:58 -0400

    wip

--- tests/test_python_type_to_typescript.py
+++ tests/test_python_type_to_typescript.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from decimal import Decimal
 import unittest
 
 from py2ts.python2ts import python_type_to_typescript
@@ -12,3 +13,6 @@ class Enum2TsTestCase(unittest.TestCase):
         assert python_type_to_typescript(float) == "number"
         assert python_type_to_typescript(datetime) == "string"
         assert python_type_to_typescript(date) == "string"
+        assert python_type_to_typescript(Decimal) == "string"
+        assert python_type_to_typescript(dict) == "{}"
+        assert python_type_to_typescript(list) == "Array<any>"
